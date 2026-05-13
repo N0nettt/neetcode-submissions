95fbaac53fd779e23ce9581a063a13bddcb993d2
Luka Urosevic <108677250+N0nettt@users.noreply.github.com>
2026-05-13 14:08:22 +0200

    Add: pow-x-n - submission-1

---
+++ Data Structures & Algorithms/pow-x-n/submission-1.py
@@ -0,0 +1,28 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        
+        def power(x, n):
+            if x == 0:
+                return 0
+            if n == 0:
+                return 1
+
+            half = power(x, n//2)
+
+            if n % 2 == 0:
+                return half*half
+            else:
+                return half*half*x
+        
+        
+        res = 1
+        if n < 0:
+            res = power(x, -n)
+            res = 1/res
+        else:
+            res = power(x, n)
+        
+        return res
+
+
+            
